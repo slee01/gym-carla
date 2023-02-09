390491ac173f8e71d53c5fac584f0248ea0209e4
Sang-Hyun Lee <slee01@snu.ac.kr>
2023-02-09 17:54:56 +0900

    add lane_change function where we generate lane_change path and use it to set global_plan of local_planner

--- agents/navigation/basic_agent.py
+++ agents/navigation/basic_agent.py
@@ -297,3 +297,25 @@ class BasicAgent(Agent):
         """(De)activates the checks for stop signs"""
         self._ignore_vehicles = active
 
+    def lane_change(self, direction, same_lane_time=0, other_lane_time=0, lane_change_time=2):
+        """
+        Changes the path so that the vehicle performs a lane change.
+        Use 'direction' to specify either a 'left' or 'right' lane change,
+        and the other 3 fine tune the maneuver
+        """
+        speed = self._vehicle.get_velocity().length()
+        path = self._generate_lane_change_path(
+            self._map.get_waypoint(self._vehicle.get_location()),
+            direction,
+            same_lane_time * speed,
+            other_lane_time * speed,
+            lane_change_time * speed,
+            False,
+            1,
+            self._sampling_resolution
+        )
+        if not path:
+            print("WARNING: Ignoring the lane change as no path was found")
+
+        self.set_global_plan(path)
+        
