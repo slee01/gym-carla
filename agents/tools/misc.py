fffb8a90a48a3e7d9751c2e42f70dda433b4c753
Sang-Hyun Lee <slee01@snu.ac.kr>
2023-02-09 17:08:28 +0900

    add several practical functions to tools/misc.py

--- agents/tools/misc.py
+++ agents/tools/misc.py
@@ -41,6 +41,67 @@ def get_speed(vehicle):
     vel = vehicle.get_velocity()
     return 3.6 * math.sqrt(vel.x ** 2 + vel.y ** 2 + vel.z ** 2)
 
+def get_trafficlight_trigger_location(traffic_light):
+    """
+    Calculates the yaw of the waypoint that represents the trigger volume of the traffic light
+    """
+    def rotate_point(point, radians):
+        """
+        rotate a given point by a given angle
+        """
+        rotated_x = math.cos(radians) * point.x - math.sin(radians) * point.y
+        rotated_y = math.sin(radians) * point.x - math.cos(radians) * point.y
+
+        return carla.Vector3D(rotated_x, rotated_y, point.z)
+
+    base_transform = traffic_light.get_transform()
+    base_rot = base_transform.rotation.yaw
+    area_loc = base_transform.transform(traffic_light.trigger_volume.location)
+    area_ext = traffic_light.trigger_volume.extent
+
+    point = rotate_point(carla.Vector3D(0, 0, area_ext.z), math.radians(base_rot))
+    point_location = area_loc + carla.Location(x=point.x, y=point.y)
+
+    return carla.Location(point_location.x, point_location.y, point_location.z)
+
+
+def is_within_distance(target_transform, reference_transform, max_distance, angle_interval=None):
+    """
+    Check if a location is both within a certain distance from a reference object.
+    By using 'angle_interval', the angle between the location and reference transform
+    will also be tkaen into account, being 0 a location in front and 180, one behind.
+    :param target_transform: location of the target object
+    :param reference_transform: location of the reference object
+    :param max_distance: maximum allowed distance
+    :param angle_interval: only locations between [min, max] angles will be considered. This isn't checked by default.
+    :return: boolean
+    """
+    target_vector = np.array([
+        target_transform.location.x - reference_transform.location.x,
+        target_transform.location.y - reference_transform.location.y
+    ])
+    norm_target = np.linalg.norm(target_vector)
+
+    # If the vector is too short, we can simply stop here
+    if norm_target < 0.001:
+        return True
+
+    # Further than the max distance
+    if norm_target > max_distance:
+        return False
+
+    # We don't care about the angle, nothing else to check
+    if not angle_interval:
+        return True
+
+    min_angle = angle_interval[0]
+    max_angle = angle_interval[1]
+
+    fwd = reference_transform.get_forward_vector()
+    forward_vector = np.array([fwd.x, fwd.y])
+    angle = math.degrees(math.acos(np.clip(np.dot(forward_vector, target_vector) / norm_target, -1., 1.)))
+
+    return min_angle < angle < max_angle
 
 def is_within_distance_ahead(target_location, current_location, orientation, max_distance):
     """
@@ -94,7 +155,17 @@ def distance_vehicle(waypoint, vehicle_transform):
 
     return math.sqrt(dx * dx + dy * dy)
 
-
+def compute_distance(location_1, location_2):
+    """
+    Euclidean distance between 3D points
+        :param location_1, location_2: 3D points
+    """
+    x = location_2.x - location_1.x
+    y = location_2.y - location_1.y
+    z = location_2.z - location_1.z
+    norm = np.linalg.norm([x, y, z]) + np.finfo(float).eps
+    return norm
+    
 def vector(location_1, location_2):
     """
     Returns the unit vector from location_1 to location_2
