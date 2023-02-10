97865f60c3c83ee6d408ef86c2c6cecc62930f65
Sang-Hyun Lee <slee01@snu.ac.kr>
2023-02-10 10:42:56 +0900

    import CarlaEnv and RoundAboutEnv

--- gym_carla/envs/__init__.py
+++ gym_carla/envs/__init__.py
@@ -1,2 +1,2 @@
-# from gym_carla.envs.carla_env import CarlaEnv
-from gym_carla.envs.carla_env import RoundAboutEnv
+from gym_carla.envs.carla_env import CarlaEnv
+from gym_carla.envs.roundabout_env import RoundAboutEnv
