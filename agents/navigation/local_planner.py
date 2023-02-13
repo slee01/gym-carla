b4409386badb75ab390a1554148f1b4bb7d75bec
Sang-Hyun Lee <slee01@snu.ac.kr>
2023-02-13 15:26:55 +0900

    delete deprecated function get_waypoints in local_planner class

--- agents/navigation/local_planner.py
+++ agents/navigation/local_planner.py
@@ -319,50 +319,6 @@ class LocalPlanner(object):
 
         self._stop_waypoint_creation = stop_waypoint_creation
 
-#   def _get_waypoints(self):
-#     """
-#     Get waypoints composed of (x,y,z) sequence from current vehicle position to 
-
-#     :param debug: boolean flag to activate waypoints debugging
-#     :return:
-#     """
-
-#     # not enough waypoints in the horizon? => add more!
-#     if len(self._waypoints_queue) < int(self._waypoints_queue.maxlen * 0.5):
-#       self._compute_next_waypoints(k=100)
-
-#     #   Buffering the waypoints
-#     while len(self._waypoint_buffer)<self._buffer_size:
-#       if self._waypoints_queue:
-#         self._waypoint_buffer.append(
-#           self._waypoints_queue.popleft())
-#       else:
-#         break
-
-#     waypoints=[]
-
-#     for i, (waypoint, _) in enumerate(self._waypoint_buffer):
-#       waypoints.append([waypoint.transform.location.x, waypoint.transform.location.y, waypoint.transform.rotation.yaw])
-
-#     # current vehicle waypoint
-#     self._current_waypoint = self._map.get_waypoint(self._vehicle.get_location())
-#     # target waypoint
-#     self._target_waypoint, self.target_road_option = self._waypoint_buffer[0]
-
-#     # purge the queue of obsolete waypoints
-#     vehicle_transform = self._vehicle.get_transform()
-#     max_index = -1
-
-#     for i, (waypoint, _) in enumerate(self._waypoint_buffer):
-#       if distance_vehicle(
-#           waypoint, vehicle_transform) < self._min_distance:
-#         max_index = i
-#     if max_index >= 0:
-#       for i in range(max_index - 1):
-#         self._waypoint_buffer.popleft()
-
-#     return waypoints    
-
     def run_step(self, debug=True):
         """
         Execute one step of local planning which involves running the longitudinal and lateral PID controllers to
