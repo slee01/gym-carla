bbd7f9feb7b5ba14fd1d7fe800788eee3f2416db
Sang-Hyun Lee <slee01@snu.ac.kr>
2023-02-13 12:19:09 +0900

    define speed_delta in each behavior_type

--- agents/navigation/behavior_types.py
+++ agents/navigation/behavior_types.py
@@ -7,6 +7,7 @@
 class Cautious(object):
     """Class for Cautious agent."""
     max_speed = 40
+    speed_delta = 3
     speed_lim_dist = 6
     speed_decrease = 12
     safety_time = 3
@@ -18,6 +19,7 @@ class Cautious(object):
 class Normal(object):
     """Class for Normal agent."""
     max_speed = 50
+    speed_delta = 5
     speed_lim_dist = 3
     speed_decrease = 10
     safety_time = 3
@@ -29,6 +31,7 @@ class Normal(object):
 class Aggressive(object):
     """Class for Aggressive agent."""
     max_speed = 70
+    speed_delta = 10
     speed_lim_dist = 1
     speed_decrease = 8
     safety_time = 3
