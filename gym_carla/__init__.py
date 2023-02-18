5afd4b72b7397dbe42e6c6d0f302641fe0edf34e
Sang-Hyun Lee <slee01@snu.ac.kr>
2023-02-18 21:39:51 +0900

    register added carla env in __init__.py

--- gym_carla/__init__.py
+++ gym_carla/__init__.py
@@ -4,3 +4,13 @@ register(
     id='roundabout-v0',
     entry_point='gym_carla.envs:RoundAboutEnv',
 )
+
+register(
+    id='lanechange-v0',
+    entry_point='gym_carla.envs:LaneChangeEnv',
+)
+
+register(
+    id='intersection-v0',
+    entry_point='gym_carla.envs:IntersectionEnv',
+)
